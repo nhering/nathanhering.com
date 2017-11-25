eabcdd0ec4892abb5acf37d720b58a755e76557e
nhering <nhering1@gmail.com>
2017-11-24 18:29:03 -0800

    UI in place for breakout

---
+++ NathanHering/js/breakout/breakout.js
@@ -0,0 +1,32 @@
+﻿var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var lives = canvas.width;
+var score = canvas.height;
+var blocksLeft = randomNumber();
+
+function clearScreen(canvas) {
+    ctx.fillStyle = "#223322";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function upadateScoreBoard(lives, score, blocksLeft) {
+    document.getElementById("lives").innerHTML = lives;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("blocksLeft").innerHTML = blocksLeft;
+}
+
+function drawPaddle() {
+    ctx.fillStyle = "#ddffdd";
+    ctx.fillRect(10, 40, 30, 6);
+}
+
+function randomNumber() {
+    return Math.floor((Math.random() * 10) + 1);
+}
+
+function loop() {
+}
+
+upadateScoreBoard(lives, score, blocksLeft);
+clearScreen(canvas);
+drawPaddle();
