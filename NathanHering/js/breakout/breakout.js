4d2ee5b162c5214ad52aceee3e381c26da839705
nhering <nhering1@gmail.com>
2017-11-27 08:03:43 -0800

    Breakout layout

--- NathanHering/js/breakout/breakout.js
+++ NathanHering/js/breakout/breakout.js
@@ -1,40 +1,74 @@
 ﻿/// <reference path="paddle.js" />
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+//var canvas = document.getElementById("canvas");
+//var ctx = canvas.getContext("2d");
 var lives = canvas.width;
 var score = canvas.height;
-var blocksLeft = randomNumber();
+var blocksDestroyed = document.getElementById('blocksDestroyed').innerHTML;
 
 //import * as paddle '~/js/breakout/paddle';
 
-function drawScreen(canvas) {
-    ctx.fillStyle = "#223322";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    var gradient = ctx.createLinearGradient(0, 400, 0, 490);
-    gradient.addColorStop(0, '#223322');
-    gradient.addColorStop(1, '#101010');
-    ctx.fillStyle = gradient;
-    ctx.fillRect(0, 400, 785, 90);
+var game = {};
+
+function breakout() {
+    game.phase = 'ready';
+    game.playing = false;
+    game.level = 1;
+    game.win = false;
+    game.score = 0;
+
+    game.bricks = {};
+    game.paddle = {};
+    game.ball = {};
+
+    game.addEventListener('keyup', function (e) { game.keyCode = e.keyCode; }, false);
+    game.timer = setInterval(function () { draw(); update() }, 40);
 }
 
-function upadateScoreBoard(lives, score, blocksLeft) {
-    document.getElementById("lives").innerHTML = lives;
-    document.getElementById("score").innerHTML = score;
-    document.getElementById("blocksLeft").innerHTML = blocksLeft;
+function ready() {
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    drawScreen(canvas, ctx);
+    drawPaddle(canvas, ctx);
+    window.addEventListener('keydown', keyListener(e.key), false);
+    _blocksDestroyed = 4;
+    play();
+
 }
 
-function drawPaddle() {
-    ctx.fillStyle = "#dcdfdc";
-    ctx.fillRect(10, 450, 80, 20);
-}
+    function keyListener(e) {
+        switch (e) {
+        }
+    }
 
-function randomNumber() {
-    return Math.floor((Math.random() * 10) + 1);
-}
+    function play() {
+        document.getElementById('playButton').style.display = 'none';
+        document.getElementById('pauseButton').style.display = 'block';
+    }
 
-function loop() {
-}
+    function pause() {
+        document.getElementById('pauseButton').style.display = 'none';
+        document.getElementById('playButton').style.display = 'block';
+    }
 
-upadateScoreBoard(lives, score, blocksLeft);
-drawScreen(canvas);
-drawPaddle();
+    function drawScreen(canvas, ctx) {
+        ctx.fillStyle = "#223322";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        var gradient = ctx.createLinearGradient(0, 400, 0, 490);
+        gradient.addColorStop(0, '#223322');
+        gradient.addColorStop(1, '#101010');
+        ctx.fillStyle = gradient;
+        ctx.fillRect(0, 400, 785, 90);
+    }
+
+    function upadateScoreBoard(lives, score, blocksLeft) {
+        document.getElementById("lives").innerHTML = lives;
+        document.getElementById("score").innerHTML = score;
+        document.getElementById("blocksDestroyed").innerHTML = blocksDestroyed;
+    }
+
+    function drawPaddle(canvas, ctx) {
+        ctx.fillStyle = "#dcdfdc";
+        ctx.fillRect(10, 455, 80, 20);
+    }
+
+    upadateScoreBoard(lives, score, blocksLeft);
