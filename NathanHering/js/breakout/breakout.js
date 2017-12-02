325dfa55655416d868a74e82babbf1e538be114b
nhering <nhering1@gmail.com>
2017-12-02 10:56:30 -0800

    fixed play button issue

--- NathanHering/js/breakout/breakout.js
+++ NathanHering/js/breakout/breakout.js
@@ -2,6 +2,10 @@
 var ctx = canvas.getContext("2d");
 var game = {};
 
+//----------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------DEFINITIONS--
+//----------------------------------------------------------------------------------------
+
 function Breakout() {
     game.mode = 'ready';
     game.level = 1;
@@ -10,9 +14,10 @@ function Breakout() {
     game.blocksDestroyed = 0;
     game.counter = 0;
     game.mouseX = 335;
-    window.addEventListener('keydown', function (e) { keyListener(e) }, false);
+    game.refresh = 50;
+    window.addEventListener('keyup', function (e) { keyListener(e) }, false);
     window.addEventListener('mousemove', function (e) { game.mouseX = e.clientX; }, false);
-    game.timer = setInterval(function () { draw(); update() }, 40);
+    game.timer = setInterval(function () { draw(); update() }, game.refresh);
 }
 
 function Brick(x,y,h) {
@@ -32,29 +37,51 @@ function Brick(x,y,h) {
 
 function keyListener(e) {
     switch (e.keyCode) {
-        case 13 : //enter
-            play();
+        case 13: //PLAY & CONTINUE (enter)
+            if (game.mode == 'play') { break; }
+            if (game.mode == 'pause') {
+                Continue();
+            }
+            else {
+                play();
+            }
             break;
-        case 32: //space bar
-            pause();
+        case 32: //PAUSE (space bar)
+            if (game.mode == 'play') {
+                pause();
+            }
+            break;
+        default:
             break;
     }
 }
 
 function play() {
+    game.mode = 'play';
     document.getElementById('playButton').style.display = 'none';
     document.getElementById('pauseButton').style.display = 'block';
-    document.getElementById('maskingLayer').style.display = 'none';
     document.getElementById('playPause').style.display = 'none';
-    game.mode = 'play';
+    document.getElementById('canvas').style.cursor = 'none';
 }
 
 function pause() {
-    document.getElementById('pauseButton').style.display = 'none';
-    document.getElementById('playButton').style.display = 'block';
-    document.getElementById('maskingLayer').style.display = 'block';
-    document.getElementById('playPause').style.display = 'block';
     game.mode = 'pause';
+    document.getElementById('pauseButton').style.display = 'none';
+    document.getElementById('continueButton').style.display = 'block';
+    document.getElementById('continueModal').style.display = 'block';
+    document.getElementById('canvas').style.cursor = 'default';
+}
+
+function Continue() {
+    game.mode = 'play';
+    document.getElementById('pauseButton').style.display = 'block';
+    document.getElementById('continueButton').style.display = 'none';
+    document.getElementById('continueModal').style.display = 'none';
+    document.getElementById('canvas').style.cursor = 'none';
+}
+
+function setStyle(id, style, value) {
+    document.getElementById(id).style[style] = value;
 }
 
 //----------------------------------------------------------------------------------------
@@ -71,6 +98,8 @@ function draw() {
             game.counter += 1;
             drawScreen();
             drawPaddle();
+            break;
+        case 'pause':
             break;
     }
 }
@@ -89,7 +118,7 @@ function drawPaddle() {
     var x = game.mouseX - canvas.getBoundingClientRect().left - 40;
     if (x <= 0) { x = 0 };
     if (x >= 670) { x = 670 };
-    if (game.mode = 'ready') { x = 335; };
+    if (game.mode == 'ready') { x = 335; };
     ctx.fillStyle = "#dcdfdc";
     ctx.fillRect(x, 455, 80, 20);
 }
@@ -99,6 +128,10 @@ function drawPaddle() {
 //----------------------------------------------------------------------------------------
 
 function update() {
+    switch (game.mode) {
+        case 'play':
+            break;
+    }
     upadateScoreBoard();
 }
 
@@ -106,4 +139,15 @@ function upadateScoreBoard() {
     document.getElementById("lives").innerHTML = game.mode;
     document.getElementById("score").innerHTML = game.counter;
     document.getElementById("blocksDestroyed").innerHTML = game.blocksDestroyed;
+    document.getElementById("time").innerHTML = timer(game.count);
+}
+
+//----------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------SUBROUTINES--
+//----------------------------------------------------------------------------------------
+
+function timer(count) {
+    var s = count / 60;
+    var m = s / 60;
+    var h = m / 60;
 }
