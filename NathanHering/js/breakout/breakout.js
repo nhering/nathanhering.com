f76fb691fcbb91c278f3c00471009952aa74d503
nhering <nhering1@gmail.com>
2017-12-17 10:47:38 -0800

    Collision detection for bricks added

--- NathanHering/js/breakout/breakout.js
+++ NathanHering/js/breakout/breakout.js
@@ -13,9 +13,8 @@ var paddle = {};
 var ball = {};
 var bricks = [];
 
-
 function Breakout() {
-    game.ball = [375, 245, 7, 0, 2 * Math.PI, '#dcdfdc', 0, 0, 10];//[0]xCoord, [1]yCoord, [2]radius, [3]startAngle, [4]endAngle, [5]color, [6]xVelocity, [7]yVelocity, [8]speed
+    game.ball = [375, 245, 7, 0, 2 * Math.PI, '#dcdfdc', 0, 0, 7];//[0]xCoord, [1]yCoord, [2]radius, [3]startAngle, [4]endAngle, [5]color, [6]xVelocity, [7]yVelocity, [8]speed
     game.mode = 'ready';
     game.level = 1;
     game.tries = 3;
@@ -23,7 +22,7 @@ function Breakout() {
     game.bricksDestroyed = 0;
     game.counter = 0;
     game.mouseX = 335;
-    game.refresh = 50;
+    game.refresh = 40;
     window.addEventListener('keyup', function (e) { keyListener(e) }, false);
     window.addEventListener('mousemove', function (e) { game.mouseX = e.clientX; }, false);
     game.timer = setInterval(function () { draw(); update() }, game.refresh);
@@ -42,6 +41,7 @@ function keyListener(e) {
             else { play(); }
             break;
         case 32: //PAUSE (space bar)
+            if (game.mode == 'pause') { resume(); }
             if (game.mode == 'play') { pause(); }
             break;
         default:
@@ -116,12 +116,13 @@ function drawScreen() {
 function drawPaddle() {
     paddle.top = 455;
     paddle.width = 80;
+    paddle.height = 20;
     var right = canvas.clientWidth - paddle.width;
     paddle.x = game.mouseX - canvas.getBoundingClientRect().left - (paddle.width/2);
     if (paddle.x <= 0) { paddle.x = 0 };
     if (paddle.x >= right) { paddle.x = right };
     ctx.fillStyle = '#dcdfdc';
-    ctx.fillRect(paddle.x, paddle.top, paddle.width, 20);
+    ctx.fillRect(paddle.x, paddle.top, paddle.width, paddle.height);
 }
 
 function drawBall(x, y) {
@@ -158,7 +159,6 @@ function update() {
         case 'play':
             game.counter++;
             upadateScoreBoard();
-            handleCollisions();
             updateBall();
             break;
     }
@@ -172,12 +172,8 @@ function upadateScoreBoard() {
 }
 
 function updateBall() {
-    var b = game.ball;
-    b[0] += b[6];
-    b[1] += b[7];
-}
+    var collide = false;
 
-function handleCollisions() {
     var b = game.ball;
     //X coordinates
     var baL = b[0] - b[2];//left
@@ -188,46 +184,137 @@ function handleCollisions() {
     var baY = b[1];//center y
     var baB = b[1] + b[2];//bottom
 
+    var pT = paddle.top;
+    var pB = paddle.top + paddle.height;//paddle's bottom
+    var pL = paddle.x;//paddle's left edge
+    var pR = paddle.x + paddle.width;//paddle's right edge
+
     //---------------------------------WALLS---
     //-----------------------------------------
-    if (b[0] <= b[2]) { //hit left side of screen
-        b[0] = b[2]; //might not like the effect this has
-        b[6] *= -1;
+    if (collide == false) {
+        if (b[0] <= b[2]) {
+            b[0] = b[2]; //might not like the effect this has
+            b[6] *= -1;
+            collide = true;
+        } //hit left side of screen
+        if (b[0] >= (canvas.clientWidth - b[2])) {
+            b[0] = canvas.clientWidth - b[2]; //might not like the effect this has
+            b[6] *= -1;
+            collide = true;
+        } //hit right side of screen
+        if (b[1] <= b[2]) {
+            b[1] = b[2]; //might not like the effect this has
+            b[7] *= -1;
+            collide = true;
+        } //hit top of screen
+        if (b[1] > (canvas.clientHeight + b[2] * 10)) {
+            lostBall();
+            collide = true;
+        } //off bottom of screen
     }
-    if (b[0] >= (canvas.clientWidth - b[2])) { //hit right side of screen
-        b[0] = canvas.clientWidth - b[2]; //might not like the effect this has
-        b[6] *= -1;
-    }
-    if (b[1] <= b[2]) { //hit top of screen
-        b[1] = b[2]; //might not like the effect this has
-        b[7] *= -1;
-    }
-    if (b[1] > (canvas.clientHeight + b[2]*10)) { //off bottom of screen
-        lostBall();
-    }
+
     //--------------------------------PADDLE---
     //-----------------------------------------
-    pT = paddle.top;
-    pB = paddle.top + paddle.height;//paddle's bottom
-    pL = paddle.x;//paddle's left edge
-    pR = paddle.x + (paddle.width);//paddle's right edge
-    //if ball's center is between paddle's left and right and ball's bottom is hitting the paddle's top, bounce up
-    if ((b[0] >= pL && b[0] <= pR) && ((b[1]+b[2]) > pT)) {
-        //todo: make ball bounce at an angle depending on where it hits the paddle
-        b[7] *= -1;
-        game.score += 1;
+    if (collide == false) {
+        if (baX >= pL && baX <= pR && baB >= pT && baB < pB) {
+            //TODO: make ball bounce at an angle depending on where it hits the paddle
+            if (baB > pT) { b[1] = pT - b[2] };
+            b[7] *= -1;
+            game.score += 1;
+            collide = true;
+        } //hit top of paddle
     }
-    //This is glitchy. It causes the ball to get stuck bouncing up and down inside the paddle
-    //if ((((b[0]+b[2] >= pL) && (b[0]-b[2] < pR)) || ((b[0]-b[2] <= pR) && (b[0]+b[2] > pL))) && ((b[1] >= pT) && (b[1] <= pB))) {
-    //    b[6] *= -1;
-    //}
-
 
     //--------------------------------BRICKS---
     //-----------------------------------------
-    for (var i = 0; i < bricks.length; i++) {
-        //if (bricks[i])
+    //[0] = x, [1] = y, [2] = width, [3] = height, [4] = color, [5] = health, [6] = powerUp
+    if (collide == false) {
+        for (var i = 0; i < bricks.length; i++) {
+            if (b[6] > 0 && b[7] >= 0) {
+                if (hitTop(i)) {
+                    break;
+                } else if (hitLeft(i)) {
+                    break;
+                }
+            } else if (b[6] > 0 && b[7] <= 0) {
+                if (hitBottom(i)) {
+                    break;
+                } else if (hitLeft(i)) {
+                    break;
+                }
+            } else if (b[6] < 0 && b[7] >= 0) {
+                //top,right
+                if (hitTop(i)) {
+                    break;
+                } else if (hitRight(i)){
+                    break;
+                }
+            } else if (b[6] < 0 && b[7] <= 0) {
+                //bottom,right
+                if (hitBottom(i)) {
+                    break;
+                } else if (hitRight(i)) {
+                    break;
+                }
+            }
+        }
     }
+
+    function hitTop(i) {
+        if (baX >= bricks[i][0] &&
+            baX <= bricks[i][0] + bricks[i][2] &&
+            baB > bricks[i][1] &&
+            baT < bricks[i][1]) {
+            b[7] *= -1;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function hitRight(i) {
+        if (baY >= bricks[i][1] &&
+            baY <= bricks[i][1] + bricks[i][3] &&
+            baL <= bricks[i][0] + bricks[i][2] &&
+            baR >= bricks[i][0] + bricks[i][2]) {
+            b[6] *= -1;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function hitBottom(i) {
+        if (baX >= bricks[i][0] &&
+            baX <= bricks[i][0] + bricks[i][2] &&
+            baT <= bricks[i][1] + bricks[i][3] &&
+            baB >= bricks[i][1] + bricks[i][3]) {
+            b[7] *= -1;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function hitLeft(i) {
+        if (baY >= bricks[i][1] &&
+            baY <= bricks[i][1] + bricks[i][3] &&
+            baR >= bricks[i][0] &&
+            baL <= bricks[i][0]) {
+            b[6] *= -1;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function doDamage(i) {
+
+    }
+
+    //move the ball
+    b[0] += b[6];
+    b[1] += b[7];
 }
 
 //----------------------------------------------------------------------------------------
@@ -271,8 +358,6 @@ function initializeBricks() {
     return bricks;
 }
 
-//place the ball at the center of the screen
-//make sure the ball starts off aiming toward the bottom
 function initializeBall() {
     var b = game.ball;
     var radians = ((Math.random() * 2.08) + 3.67) * -1; //set angle to between 3.67 - 5.75 radians (210 - 330 degrees)
